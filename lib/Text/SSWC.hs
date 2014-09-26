3062f9a4650153410317147d8cf3657595ca11ba
Tom Nielsen <tomn@brix>
2014-09-26 11:55:37 +0100

    working

--- lib/Text/SSWC.hs
+++ lib/Text/SSWC.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables, DeriveDataTypeable, StandaloneDeriving #-}
 
-{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
-{-# OPTIONS_GHC -fno-warn-orphans #-}
-
 module Text.SSWC where
 
 import Text.XmlHtml
 import Control.Applicative
 import qualified Data.ByteString as BS
 import Data.Generics hiding (Generic)
+import qualified Data.Text as T
+import qualified Data.Map.Strict as M
+import qualified Blaze.ByteString.Builder as Builder
 
 deriving instance Data Document
 deriving instance Typeable Document
@@ -27,6 +27,35 @@ loadFile :: String -> IO ()
 loadFile fnm = do
   edoc <- parseHTML fnm <$> BS.readFile fnm 
   case edoc of 
-    Right doc -> do print doc
-                    putStrLn "success"
+    Right doc -> do let ts = getTemplates doc
+                        newDoc = runTemplates ts doc
+                        docbs = Builder.toByteString $ render newDoc
+                    print ts
+                    BS.putStrLn docbs
     Left err -> putStrLn $ "error: "++err
+
+type Templates = M.Map T.Text [Node] 
+
+getTemplates :: Document -> Templates
+getTemplates = M.fromList . qquery f where
+  f (Element "template" attrs childs) 
+      = case lookup "name" attrs of
+          Just nm -> [(nm,childs)]
+          Nothing -> []
+  f _ = []
+
+runTemplates :: Templates -> Document -> Document
+runTemplates ts = qmap f where
+  f (Element "template" _ _) = Comment "template"
+  f e@(Element tagnm _ _) = case M.lookup tagnm ts of
+                              Just [elm] -> elm
+                              Just elms -> Element "div" [] elms
+                              Nothing -> e
+  f e = e
+
+
+qquery :: (Data a1, Typeable b) => (b -> [a]) -> a1 -> [a]
+qquery qf = everything (++) ([] `mkQ` qf)
+
+qmap :: (Data a, Typeable b) => (b -> b) -> a -> a
+qmap f = everywhere (mkT f)
