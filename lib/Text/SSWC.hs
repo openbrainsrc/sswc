235d5e9117c4cc3d07eaf5cf75ed127b9ffde401
Tom Nielsen <tomn@brix>
2014-09-26 11:26:52 +0100

    orphan sub instances for xmlhtml

--- lib/Text/SSWC.hs
+++ lib/Text/SSWC.hs
@@ -1,4 +1,32 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, DeriveDataTypeable, StandaloneDeriving #-}
+
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Text.SSWC where
 
 import Text.XmlHtml
+import Control.Applicative
+import qualified Data.ByteString as BS
+import Data.Generics hiding (Generic)
 
+deriving instance Data Document
+deriving instance Typeable Document
+deriving instance Data DocType
+deriving instance Typeable DocType
+deriving instance Data ExternalID
+deriving instance Typeable ExternalID
+deriving instance Data Node
+deriving instance Typeable Node
+deriving instance Data InternalSubset
+deriving instance Typeable InternalSubset
+deriving instance Data Encoding
+deriving instance Typeable Encoding
+
+loadFile :: String -> IO ()
+loadFile fnm = do
+  edoc <- parseHTML fnm <$> BS.readFile fnm 
+  case edoc of 
+    Right doc -> do print doc
+                    putStrLn "success"
+    Left err -> putStrLn $ "error: "++err
--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,12 @@
-main = do 
-  putStrLn "Hello world"
+import System.Environment
+import Text.SSWC
+
+main :: IO ()
+main = getArgs >>= dispatch
+
+dispatch :: [String] -> IO ()
+dispatch [] =   putStrLn "usage: sswc filename"
+dispatch (fnm:_) = do
+  loadFile fnm
+  return ()
+  
